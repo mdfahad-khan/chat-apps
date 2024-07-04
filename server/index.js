f16691958b95d5e1fcd6b2fc0891e30f97218a5e
fahad <evanahmedfahad@gmail.com>
2024-07-04 17:21:44 +0600

    change

--- server/index.js
+++ server/index.js
@@ -1,32 +1,50 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const connectDB = require('./config/connectDB')
-const router = require('./routes/index')
-const cookiesParser = require('cookie-parser')
-const { app, server } = require('./socket/index')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/connectDB");
+const router = require("./routes/index");
+const cookiesParser = require("cookie-parser");
+const { app, server } = require("./socket/index");
 
 // const app = express()
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
-app.use(express.json())
-app.use(cookiesParser())
+const allowedOrigins = process.env.FRONTEND_URLS
+  ? process.env.FRONTEND_URLS.split(",")
+  : [];
 
-const PORT = process.env.PORT || 8080
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      // Check if the request origin is in the allowedOrigins array
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // Allows credentials (cookies, authorization headers)
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  })
+);
 
-app.get('/',(request,response)=>{
-    response.json({
-        message : "Server running at " + PORT
-    })
-})
+app.use(express.json());
+app.use(cookiesParser());
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (request, response) => {
+  response.json({
+    message: "Server running at " + PORT,
+  });
+});
 
 //api endpoints
-app.use('/api',router)
+app.use("/api", router);
 
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
-        console.log("server running at " + PORT)
-    })
-})
+connectDB().then(() => {
+  server.listen(PORT, () => {
+    console.log("server running at " + PORT);
+  });
+});
